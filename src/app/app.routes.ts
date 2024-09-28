2dfb533838df6e14d648d63606b0922e4a3013f2
mazin safwat <mazin.safwat34@gmail.com>
2024-09-28 16:09:47 +0300

    lazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,14 @@
-import { CategoriesDetailsComponent } from './components/categories-details/categories-details.component';
-import { CategoriesComponent } from './components/categories/categories.component';
-import { DetailsMealComponent } from './components/details-meal/details-meal.component';
-import { HomeComponent } from './components/home/home.component';
 import { Routes } from '@angular/router';
-import { IngrediantsComponent } from './components/ingrediants/ingrediants.component';
-import { IngrediantsDetailsComponent } from './components/ingrediants-details/ingrediants-details.component';
-import { AreaComponent } from './components/area/area.component';
-import { AreadetailsComponent } from './components/areadetails/areadetails.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    {path:'',redirectTo:"Home",pathMatch:'full'},
-    {path:'Home',component:HomeComponent,title:'home'},
-    {path:'detailsMeal/:id',component:DetailsMealComponent,title:'details'},
-    {path:'CategoriesDetails/:name',component:CategoriesDetailsComponent,title:'details'},
-    {path:'detailsIngerediant/:name',component:IngrediantsDetailsComponent,title:'details'},
-    {path:'detailsCountry/:name',component:AreadetailsComponent,title:'details'},
-    {path:'Categories',component:CategoriesComponent,title:'categories'},
-    {path:'Ingrediants',component:IngrediantsComponent,title:'ingrediants'},
-    {path:'area',component:AreaComponent,title:'area'}
+    { path: '', redirectTo: 'Home', pathMatch: 'full' },
+    { path: 'Home',component:HomeComponent,title: 'home' },
+    { path: 'detailsMeal/:id', loadComponent: () => import('./components/details-meal/details-meal.component').then(m => m.DetailsMealComponent), title: 'details' },
+    { path: 'CategoriesDetails/:name', loadComponent: () => import('./components/categories-details/categories-details.component').then(m => m.CategoriesDetailsComponent), title: 'details' },
+    { path: 'detailsIngerediant/:name', loadComponent: () => import('./components/ingrediants-details/ingrediants-details.component').then(m => m.IngrediantsDetailsComponent), title: 'details' },
+    { path: 'detailsCountry/:name', loadComponent: () => import('./components/areadetails/areadetails.component').then(m => m.AreadetailsComponent), title: 'details' },
+    { path: 'Categories', loadComponent: () => import('./components/categories/categories.component').then(m => m.CategoriesComponent), title: 'categories' },
+    { path: 'Ingrediants', loadComponent: () => import('./components/ingrediants/ingrediants.component').then(m => m.IngrediantsComponent), title: 'ingrediants' },
+    { path: 'area', loadComponent: () => import('./components/area/area.component').then(m => m.AreaComponent), title: 'area' },
 ];
